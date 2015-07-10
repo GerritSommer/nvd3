ade2413df6ea3e321b72cc5d167d2fae5aa93996
Gerrit S. <gerrit.sommer@outlook.com>
2015-07-10 11:29:22 +0200

    Update nv.d3.js

--- nv.d3.js
+++ nv.d3.js
@@ -4750,13 +4750,13 @@ nv.models.indentedTree = function() {
 
       // Toggle children on click.
       function addClick(d, _, unshift) {
-        var controller = UI.__container__.lookup("controller:condition");
+        var controller = d.controller;
         var boundSend = controller.send.bind(controller);
         boundSend('addUserSegment', d.id);
       }
 
       function blockClick(d, _, unshift) {
-        var controller = UI.__container__.lookup("controller:condition");
+        var controller = d.controller;
         var boundSend = controller.send.bind(controller);
         boundSend('blockUserSegment', d.id);
       }
